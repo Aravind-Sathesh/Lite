a71445fb529a37e35dcd93b29723320955983584
Aravind Sathesh <aravind.sathesh@gmail.com>
2025-11-24 22:49:07 +0530

    chore: add ios pwa image

--- app/layout.tsx
+++ app/layout.tsx
@@ -62,13 +62,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={`font-sans antialiased bg-neutral-950`}>
-        {/* iOS & PWA meta tags */}
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="Lite" />
         <meta name="theme-color" content="#0a84ff" />
         <link rel="manifest" href="/manifest.json" />
+      </head>
+      <body className={`font-sans antialiased bg-neutral-950`}>
         <ColorProvider>
           {children}
           <Toaster />
